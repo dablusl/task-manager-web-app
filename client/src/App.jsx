9a9c2fd92d544383fc7d8cc327188fd9a637a3ed
dablusl <127994387+dablusl@users.noreply.github.com>
2023-09-13 05:51:16 -0500

    navigation ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { TasksPage } from './pages/TasksPage';
 import { TaskFormPage } from './pages/TaskFormPage';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
     <BrowserRouter>
+      
+      <Navigation />
+
       <Routes>
         <Route path='/' element={<Navigate to='/tasks' />} />
         <Route path='/tasks' element={<TasksPage />} />
---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+export function Navigation() {
+  return (
+    <div>
+      <Link to='/tasks'>Task App</Link>
+      <Link to='/tasks-create'>Create task</Link>
+    </div>
+  )
+}
+
