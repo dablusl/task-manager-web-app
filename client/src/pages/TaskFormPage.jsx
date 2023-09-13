af74dd1350b6b2d59c00cbee8f8e4a3f78abc690
dablusl <127994387+dablusl@users.noreply.github.com>
2023-09-13 15:37:02 -0500

    update task ok

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,6 @@
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { createTask, deleteTask } from '../api/tasks.api';
+import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.api';
 import { useNavigate, useParams } from 'react-router-dom';
 
 export function TaskFormPage() {
@@ -7,15 +8,30 @@ export function TaskFormPage() {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm();
 
   const navigate = useNavigate();
   const params = useParams();
 
   const onSubmit = handleSubmit(async (data) => {
-    await createTask(data);
+    if (params.id) await updateTask(params.id, data);
+    else await createTask(data);
+
     navigate('/tasks');
   });
+
+  useEffect(() => {
+    async function loadTask() {
+      if (params.id) {
+        const { data } = await getTask(params.id);
+        setValue('title', data.title);
+        setValue('description', data.description);
+      }
+    }
+
+    loadTask();
+  }, []);
 
   return (
     <div>
