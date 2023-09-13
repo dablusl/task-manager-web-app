6e227803e9e63270a2bf4c99f18715e8754ed92e
dablusl <127994387+dablusl@users.noreply.github.com>
2023-09-13 12:09:58 -0500

    create new task registration in databa ok

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,38 @@
+import { useForm } from 'react-hook-form';
+import { createTask } from '../api/tasks.api';
+import { useNavigate } from 'react-router-dom';
+
 export function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate()
+
+  const onSubmit = handleSubmit(async (data) => {
+    await createTask(data);
+    navigate('/tasks')
+  });
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='title'
+          {...register('title', { required: true })}
+        />
+        {errors.title && <span>the title is required</span>}
+        <textarea
+          rows='3'
+          placholder='description'
+          {...register('description', { required: true })}
+        ></textarea>
+        {errors.description && <span>the description is required</span>}
+        <button>save</button>
+      </form>
+    </div>
+  );
 }
